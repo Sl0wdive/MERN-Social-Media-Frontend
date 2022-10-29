0398bd0a68f8cee0c36e45b2e32ed449fad602a6
Sl0wdive <vladggobona@gmail.com>
2022-10-29 23:58:43 +0300

    layout first steps

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 function App() {
   return (
     <div className="Wrapper">
-      
+      <header>
+        <div className="HeaderLeft">
+          <h1>Svitlir</h1>
+        </div>
+        <div className="HeaderRight">
+          <p>
+            Username
+          </p>
+          <img width={30} height ={30} src = "/img/user.svg" />         
+        </div>
+      </header>
+      <div className="Content">
+        <h2>Стрічка новин</h2>
+        <h2>__</h2>
+      </div>
     </div>
   );
 }
