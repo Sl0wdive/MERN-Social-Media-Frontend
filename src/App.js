3e4995ae0f756c26f8b836d7411c6921686744ca
Sl0wdive <vladggobona@gmail.com>
2022-10-30 14:21:54 +0200

    start of posts layouts

--- src/App.js
+++ src/App.js
@@ -14,7 +14,25 @@ function App() {
       </header>
       <div className="Content">
         <h2>Стрічка новин</h2>
-        <h2>__</h2>
+        <div className="Post">
+          <img src=""/>
+          <p> Текст поста</p>
+          <div className="BottomPost">
+            <div className="BPLeft">
+              <button className="ButtonLike">
+                <img width={20} height ={20} src = "/img/like.png" alt="/img/likefilled.png"/>
+              </button>
+                 
+              <p>30</p>
+            </div>
+            <div className="BPRight">
+              <button className="ButtonComment">
+                <img width={20} height ={20} src = "/img/comment.png" />   
+              </button>
+              <p>5</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
