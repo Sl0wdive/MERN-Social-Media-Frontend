26328fa14aaf92f540ba5278da40f1e9f01f884f
Sl0wdive <vladggobona@gmail.com>
2022-10-30 15:50:52 +0200

    drawer created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
 function App() {
   return (
     <div className="Wrapper">
+      <div className="DrawerShadow">
+        <div className="Drawer">
+          <h2>Username</h2>
+
+          <div className="UserInfo">
+            <img width = {300} height = {300} src = "/img/user.svg"/>
+            <div className="UserMenu">
+              <button className="ButtonHome">
+                <h3>Стрічка новин</h3>
+              </button>
+              <br/>
+              <button className="ButtonMyPost">
+                <h3>Моя сторінка</h3>
+              </button>
+              <br/>
+              <button className="ButtonCreatePost">
+                <h3>Створити новий допис</h3>
+              </button>
+              <br/>
+              <button className="ButtonMyFollowings">
+                <h3>Підписки</h3>
+              </button>
+              <br/>
+              <button className="ButtonChats">
+                <h3>Діалоги</h3>
+              </button>
+              <br/>
+              <button className="ButtonLogout">
+                <h3>Вийти з акаунту</h3>
+              </button>
+              <br/>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <header>
         <div className="HeaderLeft">
           <h1>Svitlir</h1>
