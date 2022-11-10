51e63ab89eb37c2e382ec546eb44428a1f658050
Sl0wdive <vladggobona@gmail.com>
2022-11-10 18:36:34 +0200

    working with redux slices

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
 import './index.scss';
 import App from './App';
+import store from './redux/store';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import PostBlock from '../components/PostBlock';
-import axios from '../axios';
+import { fetchPosts } from '../redux/slices/posts'
 
 function Home() {
-    React.useEffect(()=>{
-        axios.get('/posts');
-      }, []);
+  const dispatch = useDispatch();
+  const { posts } = useSelector(state => state.posts);
+
+  const isPostLoading = posts.status == 'loading';
+
+  React.useEffect(()=>{
+    dispatch(fetchPosts());
+  }, []);
+
     const arr = [
         {title: 'Текст поста арр', author:"Автор", likes: 30, comments: 23, imgURL: '/img/user.svg'},
         {title: 'Текст поста арр2', author:"Автор", likes: 130, comments: 223, imgURL: '/img/user.svg'}
